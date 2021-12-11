0251de321b3f16ebd101b55386b6d0b37f7aef24
karthigovindh <gkarthi0927@gmail.com>
2021-12-11 18:47:57 +0530

    ok

---
+++ src/com/test/Employee.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Employee {
+public void add() {
+	System.out.println("add");
+
+}
+public static void main(String[] args) {
+	Employee e = new Employee();
+	e.add();
+}
+}
